14db7a9ba9ea6d534bb0f229a03b9738368917ac
Maksim Kuntsevich <max-cun4@yandex.ru>
2020-10-07 20:47:11 +0300

    init: add Program.cs

---
+++ main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace main
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
